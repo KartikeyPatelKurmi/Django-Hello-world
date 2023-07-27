ecb77dcf1405b15efa3fa116c5b2390219577eb4
KartikeyPatelKurmi <77046982+KartikeyPatelKurmi@users.noreply.github.com>
2023-07-27 11:27:27 +0530

    settings.py

--- Hello_World/Hello_World/settings.py
+++ Hello_World/Hello_World/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Hello_World.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./Hello/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
