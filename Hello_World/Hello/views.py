f2236ffb467ca928759587546f1a591398c34a39
KartikeyPatelKurmi <77046982+KartikeyPatelKurmi@users.noreply.github.com>
2023-08-15 16:43:44 +0530

    views.py

--- Hello_World/Hello/views.py
+++ Hello_World/Hello/views.py
@@ -5,10 +5,10 @@ from .models import HelloWorldModel
 def hello_world_view(request):
     hello_world_from_db = HelloWorldModel.objects.first().text # this will pick the first object from our database (in this case Hello World) and convert it in text and store it in hello_world_from_db
 
-    cached_hello_world = cache.get('hello_world_key')
+    cached_hello_world = cache.get('hello_world_key')  # hello_world_key id not found so it will return None
 
     if not cached_hello_world:
-        cache.set('hello_world_key', hello_world_from_db)
+        cache.set('hello_world_key', hello_world_from_db) # this will set the text from hello_world_from_db in cache under the key hello_world_key
 
     context = {
         'hello_world_from_db': hello_world_from_db,
