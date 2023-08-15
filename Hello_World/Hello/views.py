72fac4456807e5e54390f15957f64f294892fbc7
KartikeyPatelKurmi <77046982+KartikeyPatelKurmi@users.noreply.github.com>
2023-08-15 13:01:20 +0530

    views.py

--- Hello_World/Hello/views.py
+++ Hello_World/Hello/views.py
@@ -3,7 +3,7 @@ from django.core.cache import cache
 from .models import HelloWorldModel
 
 def hello_world_view(request):
-    hello_world_from_db = HelloWorldModel.objects.first().text
+    hello_world_from_db = HelloWorldModel.objects.first().text # this will pick the first object from our database (in this case Hello World) and convert it in text and store it in hello_world_from_db
 
     cached_hello_world = cache.get('hello_world_key')
 
